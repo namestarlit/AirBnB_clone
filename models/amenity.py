bf96345152fa490c34fe960834c40193e49a8d3f
Paul John <rainson.work@gmail.com>
2023-05-13 15:38:52 +0300

    Add `amenity.py`

---
+++ models/amenity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+amenity.py
+
+Module defines the Amenity class.
+"""
+
+from models.base_modle import BaseModel
+
+
+class Amenity(BaseModel):
+    """Represents an amenity."""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a new Amenity instance."""
+        super().__init__(*args, **kwargs)
