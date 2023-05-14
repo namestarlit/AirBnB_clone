7d6f5aa0395d2e381a71e826f19950f54111603d
Paul John <rainson.work@gmail.com>
2023-05-14 21:50:20 +0300

    Modify `base_model.py`

--- models/base_model.py
+++ models/base_model.py
@@ -23,19 +23,16 @@ class BaseModel(object):
         # Set Random unique ID to instance attribute 'id'
         self.id = str(uuid4())
 
-        # Set 'created_at' and 'updated_at' attributes.
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
         # Update instance attributes with kwargs.
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" and isinstance(value, str):
-                    setattr(self, key, datetime.fromisoformat(value))
-                if key == "updated_at" and isinstance(value, str):
-                    setattr(self, key, datetime.fromisoformat(value))
-                elif key != "__class__":
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
                     setattr(self, key, value)
+        else:
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String Representation of BaseModel class."""
@@ -51,14 +48,13 @@ class BaseModel(object):
     def to_dict(self):
         """Returns a dictionary of all the key/value of the '__dict__'."""
         # Create a new dict from instance '__dict__'
-        new_dict = dict(self.__dict__)
+        new_dict = self.__dict__.copy()
 
         # Convert 'created_at' and 'updated_at' into ISO format.
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-
-        # Set and delete the '__class__' key in the dictionary.
         new_dict['__class__'] = self.__class__.__name__
-        del new_dict['__class__']
+
+        for key, value in self.__dict__.items():
+            if key == 'created_at' or key == 'updated_at':
+                new_dict[key] = value.strftime("%Y-%m-%dT%H:%M:%S.%f")
 
         return new_dict
