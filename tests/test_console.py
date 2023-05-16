2cac67558abaee93b3c99004b71f514b5f73e6af
Paul John <rainson.work@gmail.com>
2023-05-16 17:00:44 +0300

    Modify prompt test

--- tests/test_console.py
+++ tests/test_console.py
@@ -24,7 +24,10 @@ from unittest.mock import patch
 class TestHBNBCommand_prompting(unittest.TestCase):
     """Unittests for testing prompting of the HBNB command interpreter."""
     def test_prompt_string(self):
-        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+        if sys.__stdin__.isatty():
+            self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+        else:
+            self.assertEqual("", HBNBCommand.prompt)
 
     def test_empty_line(self):
         with patch("sys.stdout", new=StringIO()) as output:
